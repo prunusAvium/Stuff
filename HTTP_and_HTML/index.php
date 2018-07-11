bef728ff6a6e47f6a08bc5bd249392be8fd46ead
prunusAvium <41112319+prunusAvium@users.noreply.github.com>
2018-07-11 15:45:12 +0300

    Add files via upload

---
+++ HTTP_and_HTML/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+
+</head>
+<body>
+<form method="post">
+    First Name:
+    <input type="text" name="first_name">
+    <button type="submit">Send</button>
+</form>
+</body>
+</html>
